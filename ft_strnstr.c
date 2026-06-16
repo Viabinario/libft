ba054fc06cf2cb12f1fc5a50bbca877b24a3ffe9
Francisco Sanchez Alvarez <francisa@c2r6s4.42madrid.com>
2026-06-16 14:34:56 +0200

    Fix ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -17,8 +17,6 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	size_t	i;
 	size_t	j;
 
-	if (!big || !little)
-		return (NULL);
 	if (!*little)
 		return ((char *)big);
 	i = 0;
